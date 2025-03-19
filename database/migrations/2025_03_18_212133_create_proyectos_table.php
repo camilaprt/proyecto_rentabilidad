67db7b69f8c05b4e92a817de388c2d4022381c3e
Camila Peralta <camilaprt@hotmail.com>
2025-03-19 20:38:11 +0100

    Comienzo proyecto con migraciones

---
+++ database/migrations/2025_03_18_212133_create_proyectos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('proyectos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre',45);
+            $table->string('descripcion',45);
+            $table->date('fecha_inicio');
+            $table->date('fecha_fin');
+            $table->tinyInteger('estado')->default(1);
+            $table->foreignId('clientes_id')->constrained('clientes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('proyectos');
+    }
+};
---
+++ database/migrations/2025_03_18_213246_create_comprobantes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comprobantes', function (Blueprint $table) {
+            $table->id();
+            $table->string('numero_comprobante',20);
+            $table->date('fecha');
+            $table->decimal('cantidad',total:8,places:2);
+            $table->enum('tipo_comprobante',['ingreso','gasto']);
+            $table->tinyInteger('estado')->default(1);
+            $table->foreignId('clientes_id')->constrained('clientes');
+            $table->foreignId('categorias_id')->constrained('categorias');
+            $table->foreignId('proyectos_id')->constrained('proyectos');
+            $table->foreignId('proveedores_id')->constrained('proveedores');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comprobantes');
+    }
+};
