6713d98c69a4359058859ce32862bb24a0087b70
Camila Peralta <camilaprt@hotmail.com>
2025-04-12 18:25:58 +0200

    Incluir email en tabla personas con migracion

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -24,6 +24,7 @@
         <a class="ml-6 text-lg font-bold text-gray-800 dark:text-gray-200" href="#">Windmill</a>
         <ul class="mt-6">
           <li class="relative px-6 py-3">
+            <span class="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg" aria-hidden="true"></span>
             <a
               class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
               href="../index.html">
