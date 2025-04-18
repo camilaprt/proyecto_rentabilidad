39780a6ae9e2fd99076f11d659f75e5eb49674e5
Camila Peralta <camilaprt@hotmail.com>
2025-04-18 18:14:57 +0200

    Diseño vista componente proyecto-detalle. Se tuvo que usar custom css.

--- resources/views/livewire/proyecto-detalle.blade.php
+++ resources/views/livewire/proyecto-detalle.blade.php
@@ -1,3 +1,74 @@
 <div>
-    PROYECTO DETALLE WORKS!
+    <h4 class="mb-4 text-lg font-semibold text-gray-600">
+        Big section cards
+    </h4>
+    <div class="contenedor-principal">
+        <!-- Bloque inicial -->
+        <div class="resumen-grid-barra">
+            <!-- Margen de rentabilidad -->
+            <div class="profit-box">
+                <h2>-65.3%</h2>
+                <p>Margen rentabilidad</p>
+            </div>
+
+            <!-- Barras -->
+            <div class="bar-section">
+                <!-- Ingresos -->
+                <div class="bar-row">
+                    <span class="bar-label">Entradas</span>
+                    <div class="bar-container">
+                        <div class="bar-fill-income"></div>
+                    </div>
+                    <span class="bar-amount">1.500,00 €</span>
+                </div>
+
+                <!-- Egresos -->
+                <div class="bar-row">
+                    <span class="bar-label">Salidas</span>
+                    <div class="bar-container">
+                        <div class="bar-fill-expense"></div>
+                    </div>
+                    <span class="bar-amount">2.483,50 €</span>
+                </div>
+            </div>
+        </div>
+
+
+
+        <!-- Bloque tres tarjetas -->
+        <div class="resumen-grid">
+            <div class="resumen-card">
+                <h2 style="color: green;">$1,500.00</h2>
+                <p>Ingresos</p>
+            </div>
+            <div class="resumen-card">
+                <h2 style="color: teal;">$2,483.50</h2>
+                <p>Egresos</p>
+            </div>
+            <div class="resumen-card">
+                <h2 style="color: red;">−$983.50</h2>
+                <p>Beneficio</p>
+            </div>
+        </div>
+
+        <!-- Listas debajo -->
+        <div class="listas-detalle">
+            <!-- Detalle ingresos -->
+            <div class="lista">
+                <h3>Detalles de Ingresos</h3>
+                <div class="lista-item"><span>Venta gabinete</span><span>€1,000</span></div>
+                <div class="lista-item"><span>Servicio montaje</span><span>€500</span></div>
+            </div>
+
+            <!-- Detalle egresos -->
+            <div class="lista">
+                <h3>Detalles de Egresos</h3>
+                <div class="lista-item"><span>Gastos bancarios</span><span>€450</span></div>
+                <div class="lista-item"><span>Sueldos</span><span>€2,033.50</span></div>
+            </div>
+        </div>
+    </div>
+
+
+
 </div>
