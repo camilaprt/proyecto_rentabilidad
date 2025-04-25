deddb7b5ac2eb82d21520e2090eb614ab4debc30
Camila Peralta <camilaprt@hotmail.com>
2025-04-25 19:36:23 +0200

    Cambiar categoria_id de migration factura a nullable, ya que venta no lo requiere

--- database/migrations/2025_03_22_203950_create_facturas_table.php
+++ database/migrations/2025_03_22_203950_create_facturas_table.php
@@ -13,12 +13,12 @@ return new class extends Migration
     {
         Schema::create('facturas', function (Blueprint $table) {
             $table->id();
-            $table->string('numero_fra',20);
+            $table->string('numero_fra', 20);
             $table->date('fecha');
-            $table->decimal('base_imp',total:8,places:2);
-            $table->decimal('total',total:8,places:2);
-             $table->foreignId('clientes_id')->constrained('clientes');
-            $table->foreignId('categorias_id')->constrained('categorias');
+            $table->decimal('base_imp', total: 8, places: 2);
+            $table->decimal('total', total: 8, places: 2);
+            $table->foreignId('clientes_id')->constrained('clientes');
+            $table->foreignId('categorias_id')->nullable()->constrained('categorias');
             $table->foreignId('proyectos_id')->constrained('proyectos');
             $table->foreignId('proveedores_id')->constrained('proveedores');
             $table->foreignId('tipo_factura_id')->constrained('tipo_facturas');
